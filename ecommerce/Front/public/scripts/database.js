d223ef869265f5434d2cadfcb9817b385d1fee22
Renan <renan.g.rodrigues5@gmail.com>
2024-12-12 23:34:05 -0300

    12/12 - noite

--- ecommerce/Front/public/scripts/database.js
+++ ecommerce/Front/public/scripts/database.js
@@ -20,7 +20,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:2,
-        tituloProduto:"Bíblia de Estudo Almeida Edição Ampliada", 
+        tituloProduto:"Bíblia de Estudo Almeida Edição Ampliada 2024 - Completa e Atualizada", 
         preco: 290.00, 
         descricao:"A nova edição da Bíblia de Estudo Almeida continua pautada pela isenção doutrinária, de modo a servir aos leitores de todas as denominações cristãs. Seu grande número de recursos possibilita aos leitores uma melhor compreensão do texto bíblico e seu contexto histórico-cultural e linguístico, social, político e religioso. Dessa forma, esta nova edição torna-se ainda mais relevante para leitura, estudo e meditação das Sagradas Escrituras. Em relação à edição anterior, são cerca de 30% a mais de notas (quase 23 mil ao todo), entre as quais figuram, pela primeira vez, mais de 100 notas escritas pelo próprio tradutor João Ferreira de Almeida no século 17.",
         imagemProduto: {
@@ -35,7 +35,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:3,
-        tituloProduto:"Bíblia Sagrada Tradução Oficial da CNBB - Cordeiro de Deus - 6ª Edição", 
+        tituloProduto:"Bíblia Sagrada Tradução Oficial da CNBB - Cordeiro de Deus - Edição 6ª", 
         preco: 67.90, 
         descricao:"A Tradução Oficial da CNBB da Bíblia Sagrada chegou em sua 6ª edição com 5 modelos de capa diferentes! Cuidadosamente traduzido com o objetivo de preservar a linguagem original e ser facilmente acessível e compreensível a todos(as) os(as) fiéis leitores(as) da atualidade, esse exemplar deseja entrar na memória e no coração de cada um(a), para alimentar a oração e inspirar a reflexão e a meditação por meio da Palavra. Este modelo de capa estampa um Cordeiro, imagem de Jesus Cristo, Cordeiro de Deus, que tira o pecado do mundo. Acabamentos: aplicação metalizada nos títulos, pintura artística nas laterais e fita de cetim para marcar a página..",
         imagemProduto: {
@@ -51,7 +51,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     
     {
         codigoProduto:4,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        tituloProduto:"A Nova Almeida Atualizada (NAA) - Edição Especial e Completa", 
         preco: 39.00, 
         descricao:"A Nova Almeida Atualizada (NAA) é uma tradução lançada em 2017 pela SBB, resultado de uma profunda revisão da consagrada tradução Almeida Revista e Atualizada. Ela apresenta um texto clássico com uma linguagem atual, seguindo a norma padrão do português escrito e falado no Brasil,Com um visual moderno e atual, capa dura e texto bíblico na Nova Almeida Atualizada (NAA), essa edição é indicada especialmente para o público jovem.",
         imagemProduto: {
@@ -67,7 +67,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:5,
-        tituloProduto:"Bíblia Sagrada - Capa leão", 
+        tituloProduto:"Bíblia Sagrada - Capa Leão - Edição Especial e Completa", 
         preco: 24.99, 
         descricao:"A Bíblia Sagrada - Capa Leão é uma edição imponente e reverente, ideal para quem busca uma obra de fé com um design marcante. Sua capa, rica em detalhes, apresenta um leão majestoso, símbolo de força, coragem e realeza, com detalhes dourados que destacam a imagem. O leão, em sua pose imponente, transmite a ideia de proteção divina e a presença de Cristo como o Leão da tribo de Judá. Esta capa não apenas realça a beleza do livro, mas também carrega um profundo significado espiritual, representando o poder e a soberania de Deus. O acabamento é refinado, com uma textura agradável ao toque, e a impressão do conteúdo segue o padrão tradicional das edições sagradas, com letras legíveis e papel de qualidade, proporcionando uma leitura confortável. Ideal para presentear ou para ter em sua biblioteca pessoal, a Bíblia Sagrada - Capa Leão é uma obra de inspiração e fé.",
         imagemProduto: {
@@ -83,12 +83,12 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:6,
-        tituloProduto:"As Virtudes Morais, armadura da vida cristã ", 
+        tituloProduto:"Bíblia King James 1611 Slim Ultrafina e Capa Tiffany - Edição Especial ", 
         preco: 35.90, 
-        descricao:"As Virtudes Morais, Armadura da Vida Cristã é uma obra que aborda de forma profunda e prática as virtudes essenciais para a vida cristã, destacando como elas funcionam como uma 'armadura espiritual', protegendo e fortalecendo o fiel em sua jornada de fé.",
+        descricao:"A Bíblia King James 1611 é uma das traduções mais reverenciadas da história cristã, conhecida por sua precisão e beleza linguística. Esta edição Slim e Ultrafina combina tradição e modernidade, ideal para quem busca praticidade sem abrir mão de um design sofisticado.",
         imagemProduto: {
             img1:"./images/products/prod06/img01.jpg",
-            img2:"./images/products/prod06/img2.png",
+            img2:"./images/products/prod06/img02.jpg",
             img3:"./images/products/prod06/img3.png",
             img4:"./images/products/prod06/img4.png",
 
@@ -99,7 +99,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:7,
-        tituloProduto:"O Deus que reconstrói sonhos", 
+        tituloProduto:"O Deus que Reconstrói Sonhos - Edição Especial - Transformação e Esperança", 
         preco: 300.00, 
         descricao:"O Deus que Reconstrói Sonhos é uma obra inspiradora que aborda o poder de Deus em restaurar e renovar as esperanças e os sonhos daqueles que passam por momentos difíceis ou de desesperança. O livro é um convite à reflexão sobre como Deus, em Sua infinita graça, pode transformar as situações mais desafiadoras em oportunidades de cura, restauração e realização dos sonhos mais profundos de cada pessoa.Com uma abordagem emocional e espiritual, o autor compartilha testemunhos, passagens bíblicas e princípios que mostram como o Senhor age para reconstruir vidas que foram abaladas por perdas, fracassos ou desilusões. A obra destaca a fidelidade de Deus e Sua capacidade de dar um novo sentido àqueles que, muitas vezes, já não acreditam que é possível alcançar seus sonhos.Ao longo do livro, o leitor é encorajado a confiar no processo divino de restauração, entender que, mesmo diante das adversidades, Deus tem um plano perfeito para cada vida, e que Ele é capaz de fazer mais do que imaginamos. 'O Deus que Reconstrói Sonhos' é uma mensagem de esperança e fé, motivando o cristão a perseverar e a se entregar à vontade de Deus, confiando que Ele pode recriar sonhos e possibilidades, transformando o que parecia perdido em algo ainda mais grandioso.É uma leitura edificante para quem busca fé renovada, consolo e uma compreensão mais profunda de como Deus pode agir de maneira sobrenatural para cumprir Seus propósitos em nossa vida.",
         imagemProduto: {
@@ -115,7 +115,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:8,
-        tituloProduto:"Deus se importa com você", 
+        tituloProduto:"Deus se Importa com Você - Edição Especial 2024 - Amor e Cuidado Divino", 
         preco: 9.48, 
         descricao:"Deus Se Importa com Você é uma obra que busca transmitir uma mensagem de conforto, esperança e encorajamento para os leitores, lembrando-lhes da profundidade do amor e cuidado de Deus por cada pessoa. O livro aborda a ideia central de que Deus está atento às nossas vidas, nossas lutas e nossas necessidades, e que Ele se importa profundamente com o bem-estar e a felicidade de cada ser humano.O autor, geralmente um líder cristão ou teólogo, utiliza passagens bíblicas e ensinamentos cristãos para ilustrar que, apesar das dificuldades e adversidades da vida, os cristãos podem confiar que Deus está sempre presente e disposto a oferecer seu auxílio, consolo e amor incondicional. O livro enfatiza que, independentemente de nossas falhas ou momentos de fragilidade, Deus nunca nos abandona e está sempre ao nosso lado.O foco do livro é incentivar os leitores a se sentirem amados e valiosos aos olhos de Deus, especialmente nos momentos de dor, dúvida ou solidão. Ao longo da obra, são exploradas maneiras práticas de como sentir a presença de Deus em nossa vida diária, como por meio da oração, meditação nas Escrituras e confiança nas promessas de Deus.Deus Se Importa com Você é uma leitura voltada para aqueles que buscam encontrar uma conexão mais profunda com Deus e uma sensação de segurança espiritual, especialmente em tempos de dificuldades emocionais ou espirituais. O livro é indicado para pessoas que necessitam de encorajamento e reafirmação de que, em qualquer circunstância, Deus cuida de nós de forma pessoal e atenta.",
         imagemProduto: {
@@ -131,7 +131,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:9,
-        tituloProduto:"Jesus, o livro", 
+        tituloProduto:"Jesus, o Livro - Edição Exclusiva 2024 - Conhecimento e Revelação Divina", 
         preco: 300.00, 
         descricao:"'Jesus, o Livro' é uma obra profundra que apresenta a vida, os ensinamentos e o impacto de Jesus Cristo de uma forma acessível e relevante para os leitores contemporâneos. O livro busca explorar, de maneira única, a figura central do cristianismo, destacando não apenas sua jornada terrena, mas também sua mensagem de amor, perdão, fé e esperança, com uma visão renovadora sobre o seu papel na história da humanidade.Através de uma narrativa envolvente, o autor oferece uma perspectiva abrangente sobre quem foi Jesus, explorando sua infância, ministério, morte e ressurreição, enquanto também examina o significado profundo de suas palavras e ações. O livro se aprofunda nas passagens bíblicas, oferecendo uma reflexão teológica rica e ao mesmo tempo prática, acessível tanto para os cristãos quanto para aqueles que buscam entender a importância histórica e espiritual de Jesus.Além da análise dos textos sagrados, 'Jesus, o Livro' também busca mostrar como os ensinamentos de Jesus continuam a influenciar vidas e culturas ao redor do mundo, sendo uma fonte de inspiração e transformação para milhões de pessoas ao longo dos séculos. O autor coloca ênfase no caráter de Jesus como líder espiritual e humanitário, desafiando os leitores a aplicar seus princípios em suas próprias vidas, promovendo um entendimento mais profundo da fé cristã e da relação com Deus.Este livro é uma leitura recomendada para quem deseja conhecer mais sobre a vida de Cristo, aprofundar sua compreensão espiritual ou até mesmo explorar a relevância de Jesus nos tempos modernos. Ele oferece uma abordagem acessível e edificante, ajudando o leitor a ver Jesus não apenas como uma figura histórica, mas como o Salvador que continua a transformar vidas até hoje.",
         imagemProduto: {
@@ -147,7 +147,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:10,
-        tituloProduto:"Decisões", 
+        tituloProduto:"Decisões: Como Fazer as Escolhas Certas na Vida - Guia de Sabedoria", 
         preco: 300.00, 
         descricao:"'A Cabana' é um romance de William P. Young que aborda questões profundas de fé, perdão e cura emocional. O livro foi publicado pela primeira vez em 2007 e rapidamente se tornou um best-seller internacional.",
         imagemProduto: {
@@ -161,7 +161,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
 
     {
         codigoProduto:11,
-        tituloProduto:"Graça Transformadora", 
+        tituloProduto:"Graça Transformadora: O Poder de Renovação e Esperança para a Vida Cristã", 
         preco: 299.00, 
         descricao:"'A Benção no Lugar Certo' é um livro escrito por Rainer Maria Rilke, um dos maiores poetas da literatura mundial. Publicado originalmente em 1923, esse trabalho não é exatamente uma narrativa, mas sim uma reflexão profunda sobre a busca por significado, identidade e o lugar do ser humano no mundo.",
         imagemProduto: {
@@ -175,12 +175,12 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
 
     {
         codigoProduto:12,
-        tituloProduto:"Livro", 
+        tituloProduto:"De Volta à Cabana: Uma Jornada de Fé, Perdão e Transformação Pessoa", 
         preco: 90.00, 
         descricao:"'De Volta à Cabana' é a continuação do famoso romance 'A Cabana', escrito por William P. Young. Publicado em 2017, o livro segue a história de Mackenzie Allen Phillips, o protagonista de 'A Cabana', mas agora com um novo foco: a jornada de cura e reflexão após os eventos do primeiro livro.",
         imagemProduto: {
-            img1:"./images/products/prod12/img01.jpg",
-            img2:"./images/products/prod12/img02.jpg",
+            img1:"./images/products/prod12/img02.jpg",
+            img2:"./images/products/prod12/img01.jpg",
             
             
         },
@@ -191,12 +191,12 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
 
     {
         codigoProduto:13,
-        tituloProduto:"O coração da bíblia", 
+        tituloProduto:"O Coração da Bíblia: Explorando as Verdades Profundas e Transformadoras das Escrituras Sagradas", 
         preco: 39.90, 
         descricao:"'O Coração da Bíblia' é uma obra escrita por John MacArthur, um renomado teólogo e pastor. O livro tem como objetivo explorar os temas centrais da Bíblia, ajudando os leitores a compreender as mensagens mais profundas das Escrituras, tanto do Antigo quanto do Novo Testamento.",
         imagemProduto: {
-            img1:"./images/products/prod13/img01.jpg",
-            img2:"./images/products/prod13/img02.jpg",
+            img1:"./images/products/prod13/img1.jpg",
+            img2:"./images/products/prod13/img2.jpg",
             
             
         },
@@ -207,25 +207,25 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
 
     {
         codigoProduto:14,
-        tituloProduto:"Bíblia do Leão da Tribo de Judá", 
+        tituloProduto:"Bíblia do Leão da Tribo de Judá: Uma Edição Especial e Inspiradora das Escrituras", 
         preco: 56.19, 
         descricao:"A Bíblia do Leão da Tribo de Judá é uma edição especial com capa dura, oferecendo robustez e durabilidade. Sua tradução segue a Nova Tradução na Linguagem de Hoje (NTLH), que visa tornar o texto bíblico mais acessível ao público contemporâneo, utilizando uma linguagem clara e simples. Essa versão é ideal para quem busca uma Bíblia prática, tanto para estudos quanto para uso diário, mantendo o equilíbrio entre profundidade teológica e clareza na comunicação.",
         imagemProduto: {
-            img1:"./images/products/prod14/img01.jpg",
+            img1:"./images/products/prod14/img01.png",
             
             
             
         },
-        categoriaProduto: 'Livro',
+        categoriaProduto: 'Bíblia',
         classificacaoProduto:"Mais_Lidos",
         exibirHome: true
     },
 
     {
         codigoProduto:15,
-        tituloProduto:"Bíblia do Leão da Tribo de Judá", 
+        tituloProduto:"Os Evangelhos Em Sua Natureza, Essência E Profundidade", 
         preco: 69.60, 
-        descricao:"A Bíblia Sagrada com Capa Leão é uma edição especial que combina tradição e modernidade. Com acabamento premium e capa dura, ela apresenta o texto da Nova Almeida Atualizada (NAA), uma tradução fiel e acessível. Esta versão é ideal para quem busca uma Bíblia com design elegante, robusta para o uso diário e fácil leitura.",
+        descricao:"Os Evangelhos Em Sua Natureza, Essência E Profundidade.",
         imagemProduto: {
             img1:"./images/products/prod15/img01.jpg",
             
